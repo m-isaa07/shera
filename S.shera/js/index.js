15c0afedc137cf81264a4e98c52691278dfe9181
Maria Isabel <143904096+m-isaa07@users.noreply.github.com>
2023-09-02 18:00:58 -0300

    Add files via upload

---
+++ S.shera/js/index.js
@@ -0,0 +1,30 @@
+
+const personagem = document.querySelectorAll ('.personagem');
+
+personagem.forEach((personagem) => {
+    personagem.addEventListener('mouseenter', () => {
+      if(window.innerWidth < 450) {
+        window.scrollTo({top: 0, behavior: 'smooth'});
+      }
+
+
+const personagemSelecionado = document.querySelector ('.selecionado');
+personagemSelecionado.classList.remove('selecionado')
+
+       personagem.classList.add('selecionado');
+
+const imagemPersonagemGrande = document.querySelector('.personagem-grande');
+
+const idPersonagem = personagem.attributes.id.value;
+imagemPersonagemGrande.src = `./S.shera/imagens/shera/${idPersonagem}-grande.png`;
+
+const nomePersonagem = document.getElementById('nome-personagem');
+
+nomePersonagem.innerText = personagem.getAttribute ('data-name');
+
+const descricaoPersonagem = document.getElementById('descricao-personagem');
+
+descricaoPersonagem.innerText = personagem.getAttribute('data-descricao')
+
+     })
+})
